ff2de8a3029adc741e20f7eef7dd4d4e6a85629a
Paulo Filipe Moreira da Silva <59426206+filipemsilv4@users.noreply.github.com>
2023-06-12 13:01:49 -0300

    Funcao def_coef

--- lab03/polinomio.c
+++ lab03/polinomio.c
@@ -1,16 +1,27 @@
 #include "lab.03.c"
-
+#include <stdlib.h>
 /* Inicializa um polinômio vazio. */
 void inicializa_polinomio(Polinomio * ap_pol){
     No* no_inicializa = (No*) calloc (1,sizeof(No));
     (*ap_pol)->prox = (*ap_pol)->antec = NULL;
 }
 
-/* Define que o coeficiente de grau grau do polinomio pol eh igual a coef. Deve manter os 
+/* Define que o coeficiente de grau g do polinomio pol eh igual a coef. Deve manter os 
  * coeficientes ordenados por grau. */
 void define_coeficiente(Polinomio pol, int grau, int coef){
 
-    if (coef == 0) return;
+    No* percorre = pol->prox;
+
+    while (grau < percorre->prox->valor.grau)
+        percorre = percorre->prox;
+    
+    if (grau == percorre->valor.grau){
+        percorre->valor.grau = grau;
+        percorre->valor.coef = coef;
+    } else {
+        No* novo_no = (No*) malloc (sizeof(No));
+
+    }
 
 
     
@@ -18,17 +29,7 @@ void define_coeficiente(Polinomio pol, int grau, int coef){
 
 /* Zera o polinomio, tornando-o um polinomio inicializado, mas igual a zero. Desaloca a memória liberada. */
 void zera(Polinomio pol){
-    pol->antec->prox = NULL;
-    No* apagando = pol->prox;
-    while (apagando->prox != NULL || apagando != NULL){   //
-        No* apagar = apagando;
-        apagando = apagando->prox;
-        free (apagar);
-    }
-    No* novo_no = (No*) malloc (sizeof(No));
-    pol->prox = pol->antec = novo_no;
-    novo_no->prox = novo_no->antec = pol;
-    novo_no->valor.coef = novo_no->valor.grau = 0;
+    return;
 }
 
 /* Computa a soma dos polinomios a e b colocando o resultado em res. 
@@ -39,7 +40,9 @@ void soma(Polinomio res, Polinomio a, Polinomio b){
 
 /* Computa a subtracao dos polinomios a e b colocando o resultado em res. 
  * Libera a memória anteriormente utilizada pelos nos descartados de res, e sobreescreve res. */
-void subtrai(Polinomio res, Polinomio a, Polinomio b);
+void subtrai(Polinomio res, Polinomio a, Polinomio b){
+
+}
 
 /* /\* Computa a multiplicacao dos polinomios a e b colocando o resultado em res.  */
 /*  * Libera a memória anteriormente utilizada pelos nos descartados de res, e sobreescreve res. *\/ */
