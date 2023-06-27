92e401eb9b8a41c97c806821c9c544a7ea3fa25c
Paulo Filipe Moreira da Silva <59426206+filipemsilv4@users.noreply.github.com>
2023-06-27 12:16:28 -0300

    Tabela HashcomDesvio
    funcionando com recursividade!

--- Testes/hashInterno.c
+++ Testes/hashInterno.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #define TABLE_SIZE 10
 #define BUCKET_SIZE 3
+#define SEM_DESVIO -1
 
-void insere_na_tabela(int chave, int pos, bucket tabela[]);
 
 int index_prox_desvio_livre = (TABLE_SIZE / 2) - 1;
 
@@ -14,9 +15,16 @@ typedef struct bucket{
     int desvio_index;  // Indice na tabela onde os próximos elementos de mesmo hash serão armazenados
 } bucket;
 
+void insere_na_tabela(int chave, int pos, bucket tabela[]);
+
+void imprime (bucket tabela[]);
+
 // Calcula o valor de hash de um dado elemento
 // TODO: Modificar para retornar somente indexes até metade da tabela
-int hash(int chave){ return chave % TABLE_SIZE; }
+
+int find_free_index(bucket tabela[]);
+
+int hash(int chave){ return chave % (TABLE_SIZE/2); }
 
 int main (void){
     bucket tabela[TABLE_SIZE];  // A tabela é um vetor de buckets
@@ -24,7 +32,7 @@ int main (void){
     // Inicializando a tabela
     for (int i = 0; i < TABLE_SIZE; ++i){
         tabela[i].qtt_chaves = 0;
-        tabela[i].desvio_index = -1;  // -1 indica que não há desvio (ainda)
+        tabela[i].desvio_index = SEM_DESVIO;  // -1 indica que não há desvio (ainda)
     }
 
     int chave;
@@ -32,14 +40,35 @@ int main (void){
         scanf("%i", &chave);
         // chamar a funcao de inserir
         insere_na_tabela(chave, hash(chave), tabela);
+        imprime(tabela);
     }
 
 }
 
 void insere_na_tabela(int chave, int pos, bucket tabela[]){
     if (tabela[pos].qtt_chaves < BUCKET_SIZE){
-        // INSERE
+        tabela[pos].chave[tabela[pos].qtt_chaves] = chave;
+        tabela[pos].qtt_chaves++;
     } else {
-        // SPILL
+        if (tabela[pos].desvio_index == SEM_DESVIO)
+            tabela[pos].desvio_index = find_free_index(tabela);
+        insere_na_tabela(chave, tabela[pos].desvio_index, tabela);
     }
+}
+int find_free_index(bucket tabela[]){
+    for (int i = TABLE_SIZE/2; i < TABLE_SIZE; ++i)
+        if(tabela[i].qtt_chaves == 0) return i;
+    printf ("Espaco insuficiente, seu programa sera encerrado\n");
+    exit(1);
+}
+void imprime (bucket tabela[]){
+
+    for(int i = 0; i < TABLE_SIZE; ++i){
+        printf("[Pos %d] ",i);
+        for (int j = 0; j < tabela[i].qtt_chaves; ++j){
+            printf("%d ", tabela[i].chave[j]);
+        }
+        printf("\n");
+    }
+    return;
 }
