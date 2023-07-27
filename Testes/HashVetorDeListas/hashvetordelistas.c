520e4e62ec90a8fe540295f5d796b8c500b25e79
Paulo Filipe Moreira da Silva <59426206+filipemsilv4@users.noreply.github.com>
2023-07-27 12:50:45 -0300

    Hash funcionando
    Falta corrigir receber nomes

--- Testes/HashVetorDeListas/hashvetordelistas.c
+++ Testes/HashVetorDeListas/hashvetordelistas.c
@@ -2,20 +2,23 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define TABLE_SIZE 15
 
+typedef unsigned long long int ulli;
+
 typedef struct{
     int idade;
-    char nome[50];
-    int cpf;
+    char* nome;
+    ulli cpf;
 }tipodado;
 
-typedef struct{
+typedef struct no{
     tipodado pessoa;
-    struct No* prox;
+    struct no* prox;
 }No, *Lista;
-
+void insere_auto (Lista tabela[]);
 void inicializa_hash (Lista tabela[]);
 No* cria_no (tipodado pessoa, No* seu_proximo);
 tipodado cria_ficha();
@@ -27,13 +30,16 @@ void imprime (Lista tabela[]);
 int main (void){
     Lista tabela[TABLE_SIZE];
     int i;
-    inicializa_hash(lista);
+    inicializa_hash(tabela);
     printf("Digite 1 para inserir na tabela\n 2 para imprimir toda a tabela\n");
-    scanf ("%d",&i);
-    switch(i){
-        case  1: insere_tabela (tabela, cria_ficha())
+    while(true){
+        scanf ("%d",&i);
+        switch(i){
+            case 1: insere_tabela (tabela, cria_ficha());
+            case 2: imprime (tabela);
+            case 3: insere_auto(tabela);
+        }
     }
-    imprime (lista);
     return 0;
 }
 int hash (int chave){
@@ -44,6 +50,7 @@ void inicializa_hash (Lista tabela[]){
         tabela[i] = NULL;
     }
 }
+
 No* cria_no (tipodado pessoa, No* seu_proximo){
     No* novo_no = malloc (sizeof(No));
     novo_no->pessoa = pessoa;
@@ -51,25 +58,49 @@ No* cria_no (tipodado pessoa, No* seu_proximo){
     novo_no->prox = seu_proximo;
     return novo_no;
 }
+
 tipodado cria_ficha(){
+    char nome[50];
+    scanf("%[^\n]",nome);
+    while(getchar() != '\n');
+    int i = strlen(nome);
     tipodado nova_pessoa;
-    gets(nova_pessoa.nome);
+    nova_pessoa.nome = malloc (i * sizeof(char));
+    strcpy(nova_pessoa.nome, nome);
     while(getchar() != '\n');
     scanf("%d", &nova_pessoa.idade);
     while(getchar() != '\n');
-    scanf("%d", &nova_pessoa.cpf);
+    scanf("%lli", &nova_pessoa.cpf);
+
+    printf("%s", nova_pessoa.nome);
+    return nova_pessoa;
 }
+
 void imprime (Lista tabela[]){
     for(int i = 0; TABLE_SIZE > i; i++){
         printf("Lista %d: ", i);
-        No* percorre = tabela[i].prox;
+        No* percorre = tabela[i];
         while(percorre != NULL){
-            printf("%s\n %d\n %d", percorre->pessoa.nome, percorre->pessoa.idade, percorre->pessoa.cpf);
+            // Lista x: [nome, idade, cpf]
+            printf("[%s, %d, %lli]", percorre->pessoa.nome, percorre->pessoa.idade, percorre->pessoa.cpf);
             percorre = percorre->prox;
         }
         printf("\n");
     }
 }
 void insere_tabela (Lista tabela[], tipodado pessoa){
-    tabela[hash(pessoa.idade)] = cria_no( pessoa, tabela[hash(pessoa.idade)].prox);
+    tabela[hash(pessoa.idade)] = cria_no( pessoa, tabela[hash(pessoa.idade)]);
+}
+void insere_auto(Lista tabela[]){
+    int i  = 0;
+    tipodado pessoa;
+    pessoa.nome = "Teste";
+    pessoa.cpf = 17786167625;
+    while(i < TABLE_SIZE){
+        pessoa.idade = i + rand() % 100;
+        i++;
+        sleep(2);
+        insere_tabela (tabela, pessoa);
+        imprime (tabela);
+    }
 }
